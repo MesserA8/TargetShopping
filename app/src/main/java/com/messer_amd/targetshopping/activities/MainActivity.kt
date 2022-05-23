672b913b1b04a22c3cc6790bfe4363f2bd68a972
MesserA8 <atihon09@gmail.com>
2022-05-23 10:50:27 +0300

    firebase added

--- app/src/main/java/com/messer_amd/targetshopping/activities/MainActivity.kt
+++ app/src/main/java/com/messer_amd/targetshopping/activities/MainActivity.kt
@@ -9,6 +9,9 @@ import androidx.preference.PreferenceManager
 import com.google.android.gms.ads.*
 import com.google.android.gms.ads.interstitial.InterstitialAd
 import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.ktx.Firebase
 import com.messer_amd.targetshopping.R
 import com.messer_amd.targetshopping.billing.BillingManager
 import com.messer_amd.targetshopping.databinding.ActivityMainBinding
@@ -19,6 +22,7 @@ import com.messer_amd.targetshopping.settings.SettingsActivity
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+    private lateinit var analytics: FirebaseAnalytics
     private lateinit var defPref: SharedPreferences
     private var currentMenuItemId = R.id.shop_list
     private var currentTheme = ""
@@ -32,6 +36,7 @@ class MainActivity : AppCompatActivity() {
         setTheme(getSelectedTheme())
 
         super.onCreate(savedInstanceState)
+        analytics = Firebase.analytics
         pref = getSharedPreferences(BillingManager.MAIN_PREF, MODE_PRIVATE)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
